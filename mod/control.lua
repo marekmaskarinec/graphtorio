fd59ae315cd9df877fcd60d726d013611890f4e2
Marek Maškarinec <marek@mrms.cz>
2025-07-28 22:50:56 +0200

    Block default lamp ui from opening
    Signed-off-by: Marek Maškarinec <marek@mrms.cz>

--- mod/control.lua
+++ mod/control.lua
@@ -1,5 +1,17 @@
-script.on_event(defines.events.on_tick,
-	function(event)
-        game.print("Hello world")
-    end
+
+script.on_event(defines.events.on_gui_opened,
+	function(ev)
+		if not ev.entity then
+			return
+		end
+
+		local e = ev.entity
+		if e.name ~= "observer" then
+			return
+		end
+
+		local player = game.players[ev.player_index]
+		player.opened = nil
+	end
 )
+
