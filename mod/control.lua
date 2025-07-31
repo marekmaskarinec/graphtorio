deab5f19a15538d78770e5f72966da8d90bd174c
Matěj Hošek <matthy.hosek@gmail.com>
2025-08-01 01:05:01 +0200

    Implement ore patch detection

--- mod/control.lua
+++ mod/control.lua
@@ -107,6 +107,49 @@ local function filter_entities_by_name(entities, name)
 	return filtered
 end
 
+-- Gets all entities in a patch the given entity is in
+local function get_ore_patch(entity)
+	local function get_neighbors(position, surface, type)
+		local directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+		local neighbors = {}
+
+		for _, dir in ipairs(directions) do
+			local pos = {position.x + dir[1], position.y + dir[2]}
+			local neighbor = surface.find_entity(type, pos)
+
+			if neighbor ~= nil then table.insert(neighbors, neighbor) end
+		end
+
+		return neighbors
+	end
+
+	local entities = {entity}
+	local entities_to_process = {entity}
+	local added_entities = {[entity.gps_tag] = true}
+
+	while #entities_to_process > 0 do
+		-- Get neighboring ore entities
+		local entity_to_process = table.remove(entities_to_process)
+		local neighbors = get_neighbors(
+			entity_to_process.position,
+			entity_to_process.surface,
+			entity_to_process.prototype.mineable_properties.products[1].name
+		)
+
+		table.insert(entities, entity_to_process)
+
+		for _, neighbor in ipairs(neighbors) do
+			-- Check if entity was not processed
+			if added_entities[neighbor.gps_tag] ~= true then
+				added_entities[neighbor.gps_tag] = true
+
+				table.insert(entities_to_process, neighbor)
+			end
+		end
+	end
+	return entities
+end
+
 -- The pub data is buffered because sometimes the game drops data when calling
 -- multiple writes per tick.
 local g_pub_buffer = ""
@@ -232,16 +275,18 @@ script.on_nth_tick(60, function(ev)
 	end
 end)
 
-script.on_event(defines.events.on_selected_entity_changed, function(event)
-	local player = game.players[event.player_index]
-	if player.selected == nil then
-		return
-	end
+script.on_event(defines.events.on_selected_entity_changed,
+    function (event)
+        local player = game.players[event.player_index]
+        if player.selected == nil then return end
 
-	local networked_entities = get_networked_entities(player.selected)
-	game.print(#networked_entities)
-	for id, entity in ipairs(networked_entities) do
-		game.print(entity)
-		log(entity)
-	end
-end)
+		local miners = filter_entities_by_type(get_networked_entities(player.selected), "mining-drill")
+
+		for _, miner in pairs(miners) do
+			get_ore_patch(miner.mining_target)
+			-- game.print("Statistics of a mineable entity")
+			-- game.print(miner.mining_target)
+			-- game.print(miner.mining_target.amount)
+			-- game.print(miner.mining_target.prototype.mineable_properties.products[1].name)
+		end
+	end)
