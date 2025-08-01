d15cea27191b69ab3faf832a5d1652d570a447e8
Matěj Hošek <matthy.hosek@gmail.com>
2025-08-01 12:34:08 +0200

    Mining-drill statistics calculation

--- mod/ore.lua
+++ mod/ore.lua
@@ -44,7 +44,50 @@ function ore.get_ore_patch(entity)
 	return entities
 end
 
--- TODO: Ore miner productivity stats
+-- Gets all ore entities reachable by a miner
+function ore.get_ore_in_range(miner)
+	local type = miner.mining_target.prototype.mineable_properties.products[1].name
+	local surface = miner.surface
+
+	local x = miner.position.x
+	local y = miner.position.y
+	local range = miner.prototype.mining_drill_radius
+
+	local area = {{math.floor(x - range), math.floor(y - range)}, {math.ceil(x + range), math.ceil(y + range)}}
+
+	local ores = surface.find_entities_filtered{area = area, name = {type}}
+	
+	return ores
+end
+
+-- Calculates the total ore across given entities
+function ore.calculate_total(entities)
+	local total = 0
+
+	for _, entity in ipairs(entities) do
+		total = total + entity.amount
+	end
+
+	return total
+end
+
+-- Gets productions stats for a given miner
+function ore.get_miner_statistics(miner)
+	local stats = {}
+
+    -- Check if given entity is a miner
+    if miner.type ~= "mining-drill" then return nil end
+
+	stats.mining_speed = (1 + miner.speed_bonus) * miner.prototype.mining_speed
+	stats.productivity = miner.productivity_bonus * stats.mining_speed
+
+	stats.output_speed = stats.mining_speed + stats.productivity
+	
+	stats.output_resource = miner.mining_target.prototype.mineable_properties.products[1].name
+	stats.expected_resources = ore.calculate_total(ore.get_ore_in_range(miner))
+
+	return stats
+end
 -- TODO: Ore patch depletion predictor
 
 return ore
